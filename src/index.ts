0b6621349201200809e6c19e55c83ccd5e48483b
molvqingtai <molvqingtai@gmail.com>
2024-03-12 13:58:27 +0800

    fix: can not import in some cases

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
-export * from './queries/bySelector'
-export * from './queries/byAttribute'
+import {
+  queryBySelector,
+  queryAllBySelector,
+  getBySelector,
+  getAllBySelector,
+  findAllBySelector,
+  findBySelector
+} from './queries/bySelector'
+
+import {
+  queryByAttribute,
+  queryAllByAttribute,
+  getByAttribute,
+  getAllByAttribute,
+  findAllByAttribute,
+  findByAttribute
+} from './queries/byAttribute'
+
+export {
+  queryBySelector,
+  queryAllBySelector,
+  getBySelector,
+  getAllBySelector,
+  findAllBySelector,
+  findBySelector,
+  queryByAttribute,
+  queryAllByAttribute,
+  getByAttribute,
+  getAllByAttribute,
+  findAllByAttribute,
+  findByAttribute
+}
